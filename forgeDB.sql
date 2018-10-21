79d45e25e43511f4fb0a0161974ca68c30016baf
volkb <32677745+volkb@users.noreply.github.com>
2018-10-21 19:34:42 -0400

    added DB script

---
+++ forgeDB.sql
@@ -0,0 +1,45 @@
+CREATE TABLE `projects` (
+  `pid` int NOT NULL AUTO_INCREMENT,
+  `plastic` varchar(10) NOT NULL,
+  `amount` varchar(50) NOT NULL,
+  `payment` int,
+  `machine` varchar(255) NOT NULL,
+  `forClass` BOOLEAN,
+  `startTime` DATETIME NOT NULL,
+  `eta` DATETIME NOT NULL,
+  `endTime` DATETIME,
+  `success` BOOLEAN,
+  `timesFailed` int,
+  `plasticBrand` varchar(255),
+  `userID` int NOT NULL,
+  PRIMARY KEY (`pid`)
+);
+
+CREATE TABLE `hardware` (
+  `mid` int  NOT NULL,
+  `inUse` BOOLEAN,
+  `status` BOOLEAN,
+  `machineName` varchar(255) NOT NULL,
+  PRIMARY KEY (`machineName`)
+);
+
+CREATE TABLE `users` (
+  `rcsID` varchar(255) NOT NULL,
+  `rin` int NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`rin`)
+);
+
+CREATE TABLE `sessions` (
+  `sessionID` int NOT NULL AUTO_INCREMENT ,
+  `userID` int NOT NULL,
+  `experation` DATETIME,
+  PRIMARY KEY (`sessionID`)
+);
+
+  ALTER TABLE `projects` ADD CONSTRAINT `fk_machine` FOREIGN KEY (`machine`) REFERENCES `hardware`(`machineName`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ALTER TABLE `projects` ADD CONSTRAINT `fk_userID`  FOREIGN KEY (`userID`) REFERENCES `users`(`rin`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ALTER TABLE `sessions` ADD CONSTRAINT `fk_userID2` FOREIGN KEY (`userID`) REFERENCES `users`(`rin`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
