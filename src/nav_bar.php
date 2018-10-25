a6de427d026a102671f5150a90ef92d5e4725c76
MichaelvMorrison <morrim5@rpi.edu>
2018-10-25 16:35:42 -0400

    Fix bootstap (#7)
    * Bootstrap, added nav_bar, style, and news.php, etc
    
    * Minor style issue with news.php

---
+++ src/nav_bar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md bg-secondary navbar-light">
+<div class="container">
+  <a class="navbar-brand" href="#">
+    <img src="logo/forgelogo revis3.png" width="65" class="d-inline-block align-top" alt="" height="50"> </a>
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto w-100">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="hours.php">Hours of Operation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="gallery.php">Gallery <!-- This will have to be changed to the new gallery -->
+          <br> </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="news.php">News</a>
+      </li>
+    </ul>
+    <a class="btn btn-primary" href="#">Become a Member!
+      <br> </a>
+  </div>
+</div>
+</nav>
--- src/news.php
+++ src/news.php
@@ -2,41 +2,11 @@
 <html>
 
 <head>
+  <?php include 'style.php';?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-
 <body>
-<!-- NAV BAR ============================================================================================== -->
-  <nav>
-    <div>
-      <a href="home.html">
-        <img src="logo/forgelogo revis3.png" width="65" alt="" height="50"> </a>
-      <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span></span>
-      </button>
-      <div id="navbarSupportedContent">
-        <ul>
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a href="hoursOfOperation.html">Hours of Operation</a>
-          </li>
-          <li>
-            <a href="gallery.html">Gallery
-              <br> </a>
-          </li>
-          <li>
-            <a href="news.html">News</a>
-          </li>
-        </ul>
-        <a href="#">Become a Member!
-          <br> </a>
-      </div>
-    </div>
-  </nav>
-<!-- /NAV BAR ============================================================================================== -->
+<?php include 'nav_bar.php';?>
 
 <!-- Media Integration goes here -->
 <!----- Footer ---->
