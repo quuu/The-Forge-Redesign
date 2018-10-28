5fc15e0075ad325e9fe97f72ea2c6fbebdec4a6c
Andrew Qu <qua@rpi.edu>
2018-10-28 17:03:03 -0400

    removed DELETE and INSERT, now uses UPDATE, TODO: password

--- src/controllers/edit_controller.php
+++ src/controllers/edit_controller.php
@@ -1,41 +1,27 @@
 <?php
 
-include_once "db_connector.php";
-$conn = dbConnect();
 if(isset($_POST['rcsID']) && isset($_POST['rin'])){
 
-    //Removing the user previously inserted
-    //TODO: Add more checking to make sure all the fields are filled
+    include_once "db_connector.php";
+    $conn = dbConnect();
+
+    //TODO: password changing
+
+    // get the rin
     $rin = $_POST['rin'];
-    $stmt = $conn->prepare('UPDATE Users SET rcsID := rcsID, firstName = :first, lastName = :last, email = :email WHERE rin = :rin');
-    $stmt->bindParam(':rin',$rin);
-    $stmt->execute();
+    //update query
+    $stmt = $conn->prepare('UPDATE Users SET rcsID := rcsID, firstName = :firstname, lastName = :lastname, email = :email WHERE rin = :rin');
+
+    //replace necessary fields
     $first = $_POST['first'];
     $last = $_POST['last'];
     $email = $_POST['email'];
     $rin = $_POST['rin'];
     $rcsID = $_POST['rcsID'];
-    $pass = password_hash($_POST['password'],PASSWORD_DEFAULT);
-
-    $conn = dbConnect();
-    //Checks to see if there are users in the database with the same rcsID
-    $stmt = $conn->prepare('SELECT * FROM Users WHERE rcsID = :rcsID');
-    $stmt->bindParam(':rcsID',$rcsID);
-    $stmt->execute();
-    $duplicate_user = $stmt->fetch();
-    if($duplicate_user){
-      //Redirects the user to the create account page again and displays an error
-      echo "<script type='text/javascript'>alert('That rcsID is unavailable!');</script>";
-      exit();
-    }
-    $stmt = $conn->prepare('INSERT INTO Users (FirstName,LastName,Email,RIN,rcsID,Password)
-    VALUES (:firstname,:lastname,:email,:RIN,:rcsID,:Password)');
     $stmt->bindParam(':firstname',$first);
     $stmt->bindParam(':lastname',$last);
     $stmt->bindParam(':email',$email);
-    $stmt->bindParam(':RIN',$rin);
-    $stmt->bindParam(':rcsID',$rcsID);
-    $stmt->bindParam(':Password',$pass);
+    $stmt->bindParam(':rin',$rin);
     $stmt->execute();
     exit();
 }else{
