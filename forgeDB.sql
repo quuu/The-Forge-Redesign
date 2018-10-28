f3c2a5bae14e9b7448d7037512a839601959f990
Andrew Qu <qua@rpi.edu>
2018-10-28 00:39:38 -0400

    Revert "Revert "updated SQL""
    This reverts commit d0cd6ec2fd9a498623aa3781449f72d675bda49c.

--- forgeDB.sql
+++ forgeDB.sql
@@ -1,9 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `forge` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `forge`;
+
 CREATE TABLE `projects` (
   `pid` int NOT NULL AUTO_INCREMENT,
   `plastic` varchar(10) NOT NULL,
   `amount` varchar(50) NOT NULL,
   `payment` int,
-  `machine` varchar(255) NOT NULL,
+  `machine` varchar(50) NOT NULL,
   `forClass` BOOLEAN,
   `startTime` DATETIME NOT NULL,
   `eta` DATETIME NOT NULL,
@@ -19,27 +22,39 @@ CREATE TABLE `hardware` (
   `mid` int  NOT NULL,
   `inUse` BOOLEAN,
   `status` BOOLEAN,
-  `machineName` varchar(255) NOT NULL,
+  `machineName` varchar(50) NOT NULL,
   PRIMARY KEY (`machineName`)
 );
 
 CREATE TABLE `users` (
   `rcsID` varchar(255) NOT NULL,
   `rin` int NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `type` varchar(10) NOT NULL,
   PRIMARY KEY (`rin`)
 );
 
+CREATE TABLE `volunteers` (
+  `vID` int NOT NULL AUTO_INCREMENT,
+  `rin` int NOT NULL,
+  `dayOfWeek` int NOT NULL,
+  `startTime` TIME NOT NULL,
+  `endTime` TIME NOT NULL,
+  PRIMARY KEY (`vID`)
+);
+
 CREATE TABLE `sessions` (
-  `sessionID` int NOT NULL AUTO_INCREMENT ,
+  `sessionID` varchar(1000) NOT NULL,
   `userID` int NOT NULL,
   `experation` DATETIME,
-  PRIMARY KEY (`sessionID`)
+  PRIMARY KEY (`sessionID`(191))
 );
 
   ALTER TABLE `projects` ADD CONSTRAINT `fk_machine` FOREIGN KEY (`machine`) REFERENCES `hardware`(`machineName`) ON DELETE NO ACTION ON UPDATE NO ACTION;
   ALTER TABLE `projects` ADD CONSTRAINT `fk_userID`  FOREIGN KEY (`userID`) REFERENCES `users`(`rin`) ON DELETE NO ACTION ON UPDATE NO ACTION;
   ALTER TABLE `sessions` ADD CONSTRAINT `fk_userID2` FOREIGN KEY (`userID`) REFERENCES `users`(`rin`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ALTER TABLE `volunteers` ADD CONSTRAINT `fk_userID3` FOREIGN KEY (`rin`) REFERENCES `users`(`rin`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
