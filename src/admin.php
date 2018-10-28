5e5a2ff83f2d712c8cc28204cdb92aeffbed27ac
Andrew Qu <qua@rpi.edu>
2018-10-28 00:51:11 -0400

    admin page

---
+++ src/admin.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Admin</title>
+</head>
+
+<body>
+  <h1>Admin</h1>
+  <div class="form">
+      <a href="./edit_user.php">Edit User</a>
+      <a href="./create_account.php">Create Account</a>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+</body>
+</html>
