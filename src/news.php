c74bc5e9b49d8c5c0c57c3e26f2c3112597af7ff
volkb <volkb@rpi.edu>
2018-10-28 22:51:07 -0400

    fixed filtering problem and created gallery page with API config

--- src/news.php
+++ src/news.php
@@ -11,7 +11,7 @@
 <!-- Media Integration goes here -->
 <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
 <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
-<ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" filter="Facebook"><h1 class="referral"><a href="https://www.juicer.io">Powered by Juicer</a></h1></ul>
+<ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" data-filter="Facebook"><h1 class="referral"><a href="https://www.juicer.io">Powered by Juicer</a></h1></ul>
 <!----- Footer ---->
   <div>
     <p>© Copyright 2018 RPI Forge - All rights reserved. </p>
