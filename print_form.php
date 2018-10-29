4b4415fa48601efc73840e9597cd2ed8141c6782
Logan Ramos <lramos15@gmail.com>
2018-10-29 16:38:52 -0400

    Fixed required issues with print form and mae it display the price to the user

--- print_form.php
+++ print_form.php
@@ -13,39 +13,32 @@ include "controllers/print_form_controller.php";
 <body>
     <form action="#" method="POST">
         <br/>
-        <label for="machine" id="machine">Machine used for print</label>
-        <select name="machine" required>
+        <label for="machine">Machine used for print</label>
+        <select name="machine" id="machine" class="required">
            <?php generateMachineDropDown() ?>
         </select>
         <br/>
         <div id="plasticInfo">
             <label for="plastic" id="plasticlabel">Plastic Type</label>
-            <select name="plastic"  id="plastictype" required>
-                <!--
-                <option value="pla">PLA ($0.07/gram)</option>
-                <option value="abs">ABS ($0.06/gram)</option>
-                <option value="nylon">Nylon ($0.10/gram)</option>
-                <option value="flexy">Flexy ($0.09/gram)</option>
-                <option value="ngen">nGen ($0.055/gram)</option>
-                <option value="resin">Resin ($0.20/mL)</option>
-                -->
+            <select name="plastic"  id="plastictype" class="required">
                 <?php generatePlasticsDropDown() ?>
             </select>
             <br/>
             <label for="amount" id="amountlabel">Amount of plastic (in g)</label>
-            <input type="text" name="amount" id="plasticamount" required/>
+            <input type="text" name="amount" id="plasticamount" class="required"/>
+            <label for="price" id="printprice"></label>
             <br/>
             <label for="brand" id = "brandlabel">Plastic Brand</label>
-            <input type="text" name="brand" id = "brand" required/>
+            <input type="text" name="brand" id = "brand" class="required"/>
             <br/>
             <label for="temp" id = "templabel">Print temperature</label>
-            <input type="text" name="temp" id = "temp" required/>
+            <input type="text" name="temp" id = "temp" class="required"/>
             <br/>
             <label for="color" id = "colorlabel">Color of Plastic</label>
-            <input type="text" name="color" id = "color" required/>
+            <input type="text" name="color" id = "color" class="required"/>
             <br/>
             <label for="time">Estimate time to completion (in HH format):</label>
-            <input type="number" name="time" min="1" max="1000" required/>
+            <input type="number" name="time" min="1" max="1000" class="required"/>
             <br/>
         </div>
         <input type="checkbox" name="forclass" value="1"/>
@@ -55,11 +48,11 @@ include "controllers/print_form_controller.php";
         <p>Your total print is under 50 grams (or 7mL). If your print has failed and has consumed less than 50 grams (or 7mL) of plastic you will not be charged
         for up to two aditional reprint attempts.</p>
         <p><strong>The volunteer present has final say. If you wish to appeal your claim, please email kronmm@rpi.edu</strong></p>
-        <input type="checkbox" name="reprintpolicy" value="agree" required/>
+        <input type="checkbox" name="reprintpolicy" value="agree" class="required"/>
         <label for="reprintpolicy"> By checking the box you agree to the reprint policy stated above.</label>
         </div>
         <label for="initials" id = "initialslabel">By initialing here, you agree to the charges entered above.</label>
-        <input type="text" name="initials" id="initials" required/>
+        <input type="text" name="initials" id="initials" class="required"/>
         <br/>
         <input type="submit" value="Submit"/>
     </form>
