16bf548983e98dd4db57eb8d57fc4b09e37ff787
MichaelvMorrison <morrim5@rpi.edu>
2018-10-31 23:22:06 -0400

    Started to style create_account - has a few issues

--- create_account.php
+++ create_account.php
@@ -2,12 +2,14 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <?php include 'style.php'?>
     <meta charset="utf-8">
     <title>Create Account</title>
   </head>
 
   <body>
-    <h1>Create Account</h1>
+    <?php include 'nav_bar.php'?>
+    <!-- <h1>Create Account</h1>
     <div class="create">
       <form action="controllers/ca_controller.php" method="post">
         <label for="first">First Name:</label>
@@ -26,7 +28,54 @@
         <br/>
         <input type="submit" name='Submit' value="Create">
       </form>
+    </div> -->
+
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <div class="card shadow-lg my-5">
+            <div class="card-body">
+              <h1 class="card-title text-center">Create Account</h1>
+              <form action="controllers/ca_controller.php" method="post">
+
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="first" placeholder="First Name" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="last" placeholder="Last Name" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <input type="email" class="form-control" name="email" placeholder="RPI E-mail" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <input type="number" class="form-control" min="660000000" max="999999999" name="rin" placeholder="RIN" required/>
+                  </div>
+
+                <div class="form-group">
+                  <input type="text" class="form-control" name="rcsID" placeholder="RCS ID" required/>
+                </div>
+
+                <div class="form-group">
+                  <input type="password" class="form-control" name="password" placeholder="Password" required/>
+                </div>
+
+                <div class="text-center">
+                  <button class="btn btn-primary btn-clock text-uppercase" type="submit" name="submit">Create</button>
+                </div>
+
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <script src="js/jquery-3.3.1.min.js"></script>
+
+
   </body>
 </html>
