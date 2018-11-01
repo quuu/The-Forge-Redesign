6ccbb0e53aacaf6cf102e6cb6aa0baac34e8bb91
MichaelvMorrison <morrim5@rpi.edu>
2018-10-31 21:52:49 -0400

    Small styling fix

--- login.php
+++ login.php
@@ -10,33 +10,36 @@
   <?php include 'nav_bar.php';?>
 
   <div class="container">
+    <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card sign_in_card my-5">
-          <div class="card-body">
-          <h1 class="card-title text-center">Sign In</h1>
-            <form action="controllers/login_controller.php" method="post">
-              <div class="form-group">
-                  <label for="rcsID">RCS ID:</label>
-                  <input type="text" class="form-control" name="rcsID" placeholder="Enter RCS ID" required autofocus />
-              </div>
-
+          <div class="card shadow-lg my-5">
+            <div class="card-body">
+            <h1 class="card-title text-center">Sign In</h1>
+              <form action="controllers/login_controller.php" method="post">
                 <div class="form-group">
-                  <label for="password">Password:</label>
-                  <input type="password" class="form-control" name="password" placeholder="Enter Password" required />
+                    <label for="rcsID">RCS ID:</label>
+                    <input type="text" class="form-control" name="rcsID" placeholder="Enter RCS ID" required autofocus />
                 </div>
 
-                <div class="custom-control custom-checkbox mb-3">
-                  <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                  <label class ="custom-control-label" for="customCheck1">Remember Password</label>
-                </div>
+                  <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" placeholder="Enter Password" required />
+                  </div>
 
-                <div class="text-center">
-                  <button class="btn  btn-primary btn-clock text-uppercase" type="submit">Sign in</button>
+                  <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                    <label class ="custom-control-label" for="customCheck1">Remember Password</label>
+                  </div>
+
+                  <div class="text-center">
+                    <button class="btn  btn-primary btn-clock text-uppercase" type="submit">Sign in</button>
+                  </div>
+                </form>
                 </div>
-              </form>
-              </div>
-          </div>
+            </div>
         </div>
+    </div>
+
       </div>
   </div>
   <?php include 'footer.php'; ?>
