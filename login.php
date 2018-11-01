752612f3ab6bd7d61ca694ceb3c91c9294f172d0
MichaelvMorrison <morrim5@rpi.edu>
2018-10-31 23:26:46 -0400

    Removed remember password

--- login.php
+++ login.php
@@ -26,11 +26,6 @@
                     <input type="password" class="form-control" name="password" placeholder="Enter Password" required />
                   </div>
 
-                  <div class="custom-control custom-checkbox mb-3">
-                    <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                    <label class ="custom-control-label" for="customCheck1">Remember Password</label>
-                  </div>
-
                   <div class="text-center">
                     <button class="btn  btn-primary btn-clock text-uppercase" type="submit">Sign in</button>
                   </div>
