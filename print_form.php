01c13ff6e6b01f4784153d24ee70961c77d534c2
MichaelvMorrison <morrim5@rpi.edu>
2018-11-16 15:23:39 -0500

    Print_form.php style (#19)
    * Random styling stuff
    
    * Linked to gallery on index.php
    
    * Deleted repeated css files
    Created style.css for our custom style
    Fixed virtual links on index
    
    * Fixed minor styling issues with includes
    Fixed issue with including both bootstrap and concise
    Largely styled
    
    * Small styling fix
    
    * Minor styling tweaks to login
    
    * Fixed form-control:focus border
    
    * Started to style create_account - has a few issues
    
    * Removed remember password
    
    * Styled Edit User
    
    * Styled print_form.php. Adjusted print_form.js and
    print_Form_controller.php accordingly
    
    * Small sizing fix
    
    * concat fix

--- print_form.php
+++ print_form.php
@@ -9,53 +9,100 @@ include "controllers/print_form_controller.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Print Job Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include 'style.php'?>
+    <?php include 'scripts.php'?>
 </head>
 <body>
-    <form action="#" method="POST">
-        <br/>
-        <label for="machine">Machine used for print</label>
-        <select name="machine" id="machine" class="required">
-           <?php generateMachineDropDown() ?>
-        </select>
-        <br/>
-        <div id="plasticInfo">
-            <label for="plastic" id="plasticlabel">Plastic Type</label>
-            <select name="plastic"  id="plastictype" class="required">
-                <?php generatePlasticsDropDown() ?>
-            </select>
-            <br/>
-            <label for="amount" id="amountlabel">Amount of plastic (in g)</label>
-            <input type="text" name="amount" id="plasticamount" class="required"/>
-            <label for="price" id="printprice"></label>
-            <br/>
-            <label for="brand" id = "brandlabel">Plastic Brand</label>
-            <input type="text" name="brand" id = "brand" class="required"/>
-            <br/>
-            <label for="temp" id = "templabel">Print temperature</label>
-            <input type="text" name="temp" id = "temp" class="required"/>
-            <br/>
-            <label for="color" id = "colorlabel">Color of Plastic</label>
-            <input type="text" name="color" id = "color" class="required"/>
-            <br/>
-            <label for="time">Estimate time to completion (in HH format):</label>
-            <input type="number" name="time" min="1" max="1000" class="required"/>
-            <br/>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-9 mx-auto">
+          <div class="card shadow-lg my-5">
+            <div class="card-body">
+              <h1 class="card-title text-center">Print Job Form</h1>
+              <form action="#" method="POST">
+
+                <label for="machine" id="machinelabel">Machine Type:</label>
+                <select name="machine" id="machine" required>
+                  <?php generateMachineDropDown() ?>
+                </select>
+
+                <div id="plasticinfo">
+                  <div class="form-group">
+                    <label for="plastic" id="plasticlabel">Plastic Type:</label>
+                    <select name="plastic" id="plastictype" required>
+                      <?php generatePlasticsDropDown() ?>
+                    </select>
+                  </div>
+
+
+                  <div class="form-group">
+                    <label for="amount" id="amountlabel">Amount of plastic (g)</label>
+                    <input type="text" class="form-control" id="plasticamount" name="amount" required/>
+                    <small id="printprice" class="form-text text-muted ml-1"></small>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="brand" id="brandlabel">Plastic Brand</label>
+                    <input type="text" class="form-control" name="brand" id="brand" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="temp" id="templabel">Print temperature</label>
+                    <input type="text" class="form-control" name="temp" id="temp" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="color" id="colorlabel">Color of Plastic</label>
+                    <input type="text" class="form-control" name="color" id="color" required/>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="time">Estimate time to completion (in HH format):</label>
+                    <input type="number" class="form-control" name="time" min="1" max="1000" required/>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <input type="checkbox" name="forclass" value="1"/>
+                  <label for="forclass">This print is for a class</label>
+                </div>
+
+                <hr id='sectiondivider1'/>
+
+
+
+                <div id="reprintpolicy">
+                  <p class="text-center"><strong>Reprint Policy:</strong></p>
+                  <p>Your total print is under 50g/7mL. If your print has failed and has consumed less than 50g/7mL of plastic you will not be charged
+                  for up to two aditional reprint attempts.</p>
+                  <p><strong>The volunteer present has final say. If you wish to appeal your claim, please email kronmm@rpi.edu</strong></p>
+
+
+                  <div class="form-group">
+                    <input type="checkbox" name="reprintpolicy" value="agree" required/>
+                    <label for="reprintpolicy"> I agree to the reprint policy.</label>
+                  </div>
+                </div>
+
+                <hr id='sectiondivider2'/>
+
+                <div class="form-group">
+                  <label for="initials" id="initialslabel">Initials</label>
+                  <input type="text" class="form-control" name="initials" id="initials" required/>
+                  <small id="initialssmall" class="form-text text-muted ml-1">By initialing here, you agree to pay the charge shown above.</small>
+                </div>
+
+                <div class="text-center">
+                  <button class="btn btn-primary btn-clock text-uppercase" type="submit" name="submit">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <input type="checkbox" name="forclass" value="1"/>
-        <label for="forclass">Is this print for a class?</label>
-        <br/>
-        <div id="reprintpolicy">
-        <p>Your total print is under 50 grams (or 7mL). If your print has failed and has consumed less than 50 grams (or 7mL) of plastic you will not be charged
-        for up to two aditional reprint attempts.</p>
-        <p><strong>The volunteer present has final say. If you wish to appeal your claim, please email kronmm@rpi.edu</strong></p>
-        <input type="checkbox" name="reprintpolicy" value="agree" class="required"/>
-        <label for="reprintpolicy"> By checking the box you agree to the reprint policy stated above.</label>
-        </div>
-        <label for="initials" id = "initialslabel">By initialing here, you agree to the charges entered above.</label>
-        <input type="text" name="initials" id="initials" class="required"/>
-        <br/>
-        <input type="submit" value="Submit"/>
-    </form>
+      </div>
+    </div>
+
+
 </body>
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/print_form.js"></script>
