6889d52b83b24d9ebe6d0fc232e03317d8043b49
Andrew Qu <qua@rpi.edu>
2018-11-21 13:41:58 -0500

    change password under admin page works

--- controllers/edit_controller.php
+++ controllers/edit_controller.php
@@ -2,11 +2,12 @@
 
 include_once "db_connector.php";
 if(isset($_POST['rcsID']) && isset($_POST['rin'])){
-
+  
+  /**
+   * if password doesn't need to be changed
+   */
+  if(!(isset($_POST['password']))){
     $conn = dbConnect();
-
-    //TODO: password changing
-
     // get the rin
     $rin = $_POST['rin'];
     //update query
@@ -25,6 +26,33 @@ if(isset($_POST['rcsID']) && isset($_POST['rin'])){
     $stmt->bindParam(':rcsID',$rcsID);
     $stmt->execute();
     exit();
+  }
+  /**
+   * if password needs to be changed
+   */
+  else{
+    $conn = dbConnect();
+    // get the rin
+    $rin = $_POST['rin'];
+    //update query
+    $stmt = $conn->prepare('UPDATE Users SET rcsID = :rcsID, firstName = :firstname, lastName = :lastname, email = :email, password = :password WHERE rin = :rin');
+
+    //replace necessary fields
+    $first = $_POST['first'];
+    $last = $_POST['last'];
+    $email = $_POST['email'];
+    $rin = $_POST['rin'];
+    $rcsID = $_POST['rcsID'];
+    $password = $_POST['password'];
+    $stmt->bindParam(':firstname',$first);
+    $stmt->bindParam(':lastname',$last);
+    $stmt->bindParam(':email',$email);
+    $stmt->bindParam(':rin',$rin);
+    $stmt->bindParam(':rcsID',$rcsID);
+    $stmt->bindParam(':password',$password);
+    $stmt->execute();
+    exit(); 
+  }
 }else{
   die("ERROR");
 }
