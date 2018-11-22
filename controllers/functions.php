5b40b3fe8c9c5a25287f9189b2f3bc8f2b40767a
volkb <volkb@rpi.edu>
2018-11-21 22:35:06 -0500

    added basic functionality to functions.php

--- controllers/functions.php
+++ controllers/functions.php
@@ -1,3 +1,66 @@
 <?php
+include_once "db_connector.php";
+
+function getRCSID(){
+    if(isset($_COOKIE['FORGE-SESSION'])){
+        $sessionID = $_COOKIE['FORGE-SESSION'];
+        $conn = dbConnect();
+        //grab the UserID (RIN) from the Session Data
+        $rin = $conn->prepare("SELECT `UserID` FROM `Sessions` WHERE `sessionID` = '$sessionID'");
+        $rin->execute();
+
+        //use RIN to get RCSID
+        $result = $conn->prepare("SELECT `rcsID` FROM `users` WHERE `rin` = '$rin'");
+        $result->execute();
+
+        //return value
+        return $result;
+    }else{
+        return 404;
+    }
+
+
+}
+
+function getName(){
+    if(isset($_COOKIE['FORGE-SESSION'])){
+        $sessionID = $_COOKIE['FORGE-SESSION'];
+        $conn = dbConnect();
+        //grab the UserID (RIN) from the Session Data
+        $rin = $conn->prepare("SELECT `UserID` FROM `Sessions` WHERE `sessionID` = '$sessionID'");
+        $rin->execute();
+
+        //use RIN to get firstName
+        $result = $conn->prepare("SELECT `firstName` FROM `users` WHERE `rin` = '$rin'");
+        $result->execute();
+
+        //return value
+        return $result;
+    }else{
+        return 404;
+    }
+
+}
+
+function getEmail(){
+    if(isset($_COOKIE['FORGE-SESSION'])){
+        $sessionID = $_COOKIE['FORGE-SESSION'];
+        $conn = dbConnect();
+        //grab the UserID (RIN) from the Session Data
+        $rin = $conn->prepare("SELECT `UserID` FROM `Sessions` WHERE `sessionID` = '$sessionID'");
+        $rin->execute();
+
+        //use RIN to get Email
+        $result = $conn->prepare("SELECT `email` FROM `users` WHERE `rin` = '$rin'");
+        $result->execute();
+
+        //return value
+        return $result;
+    }else{
+        return 404;
+    }
+
+}
+
 
 ?>
