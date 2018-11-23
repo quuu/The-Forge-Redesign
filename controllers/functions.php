dd98887e90d3710b6b65b3e9eb1bcaa81af6506c
volkb <volkb@rpi.edu>
2018-11-23 14:28:38 -0500

    resolved PDO ERRORS

--- controllers/functions.php
+++ controllers/functions.php
@@ -9,14 +9,16 @@ function getRCSID(){
         $rin = $conn->prepare("SELECT UserID FROM Sessions WHERE sessionID = :sessionID");
         $rin->bindParam(':sessionID',$sessionID);
         $rin->execute();
+        $rin_result = $rin->fetchColumn();
 
         //use RIN to get RCSID
         $result = $conn->prepare("SELECT rcsID FROM users WHERE rin = :rin");
-        $result->bindParam(':rin',$rin);
+        $result->bindParam(':rin',$rin_result);
         $result->execute();
+        $ret_result = $result->fetchColumn();
 
         //return value
-        return $result;
+        return $ret_result;
     }else{
         return 404;
     }
@@ -30,14 +32,17 @@ function getName(){
         $rin = $conn->prepare("SELECT UserID FROM Sessions WHERE sessionID = :sessionID");
         $rin->bindParam(':sessionID',$sessionID);
         $rin->execute();
+        $rin_result = $rin->fetchColumn();
 
         //use RIN to get firstName
         $result = $conn->prepare("SELECT firstName FROM users WHERE rin = :rin");
-        $result->bindParam(':rin',$rin);
+        $result->bindParam(':rin',$rin_result);
         $result->execute();
+        $ret_result = $result->fetchColumn();
 
         //return value
-        return $result;
+        //var_dump($ret_result);
+        return $ret_result;
     }else{
         return 404;
     }
@@ -51,14 +56,16 @@ function getEmail(){
         $rin = $conn->prepare("SELECT UserID FROM Sessions WHERE sessionID = :sessionID");
         $rin->bindParam(':sessionID',$sessionID);
         $rin->execute();
+        $rin_result = $rin->fetchColumn();
 
         //use RIN to get Email
         $result = $conn->prepare("SELECT email FROM users WHERE rin = :rin");
-        $result->bindParam(':rin',$rin);
+        $result->bindParam(':rin',$rin_result);
         $result->execute();
+        $ret_result = $result->fetchColumn();
 
         //return value
-        return $result;
+        return $ret_result;
     }else{
         return 404;
     }
@@ -72,14 +79,16 @@ function getPerms(){
         $rin = $conn->prepare("SELECT UserID FROM Sessions WHERE sessionID = :sessionID");
         $rin->bindParam(':sessionID',$sessionID);
         $rin->execute();
+        $rin_result = $rin->fetchColumn();
 
         //use RIN to get Perms (Type)
         $result = $conn->prepare("SELECT type FROM users WHERE rin = :rin");
-        $result->bindParam(':rin',$rin);
+        $result->bindParam(':rin',$rin_result);
         $result->execute();
+        $ret_result = $result->fetchColumn();
 
         //return value
-        return $result;
+        return $ret_result;
     }else{
         return 404;
     }
@@ -93,14 +102,16 @@ function getUses(){
         $rin = $conn->prepare("SELECT UserID FROM Sessions WHERE sessionID = :sessionID");
         $rin->bindParam(':sessionID',$sessionID);
         $rin->execute();
+        $rin_result = $rin->fetchColumn();
 
         //use RIN to get 10 most recent Projects in reverse chronological order
         $result = $conn->prepare("SELECT * FROM projects WHERE userID = :rin ORDER BY startTime DESC LIMIT 10");
-        $result->bindParam(':rin',$rin);
+        $result->bindParam(':rin',$rin_result);
         $result->execute();
+        $ret_result = $result->fetchColumn();
 
         //return value
-        return $result;
+        return $ret_result;
     }else{
         return 404;
     }
