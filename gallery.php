306ca3047074f5aad60c5daedb6b5f57783d3bd2
MichaelvMorrison <morrim5@rpi.edu>
2018-11-22 21:10:18 -0600

    Index style (#24)
    * Random styling stuff
    
    * Linked to gallery on index.php
    
    * Deleted repeated css files
    Created style.css for our custom style
    Fixed virtual links on index
    
    * Fixed minor styling issues with includes
    Fixed issue with including both bootstrap and concise
    Largely styled
    
    * Small styling fix
    
    * Minor styling tweaks to login
    
    * Fixed form-control:focus border
    
    * Started to style create_account - has a few issues
    
    * Removed remember password
    
    * Misc. Index style
    
    * DB changes, unimportant
    
    * semi colon

--- gallery.php
+++ gallery.php
@@ -5,12 +5,12 @@
   <?php include 'style.php';?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<body>
+<body class="bg-light">
   <?php include 'nav_bar.php';?>
   <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
   <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
   <div class="container" id="juicer">
-    <ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" data-filter="Instagram"><h1 class="referral"></h1></ul>
+    <ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" data-style="hip" data-filter="Instagram"><h1 class="referral"></h1></ul>
   </div>
   <?php include 'footer.php' ?>
 </body>
--- news.php
+++ news.php
@@ -5,14 +5,14 @@
   <?php include 'style.php';?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<body>
+<body class="bg-light">
 <?php include 'nav_bar.php';?>
 
 <!-- Media Integration goes here -->
 <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
 <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
 <div class="container">
-  <ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" data-filter="Facebook"><h1 class="referral"></h1></ul>
+  <ul class="juicer-feed" data-feed-id="rpimakerspace" data-style="polaroid" data-per="20" data-filter="Facebook"><h1 class="referral"></h1></ul>
 </div>
 <?php include 'footer.php' ?>
 </body>
