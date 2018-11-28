92196efa118bb3b5f08085d8d36096571698d3cd
Andrew Qu <qua@rpi.edu>
2018-11-27 20:49:30 -0500

    fixed flashing by refactoring code, no longer relies on continously clearing the page, updates the bars inline Accounts for success of project TODO: look at timesfailed and trigger the email

--- js/status_bars.js
+++ js/status_bars.js
@@ -26,7 +26,8 @@ function fetchProjects(projectsReturn){
     });
 }
 
-function status_bars(){
+function createStatusBars(){
+    // $('#statuses').empty();
    /**
      * machines[n][0] == inUSe
      * machines[n][1] == status
@@ -61,83 +62,108 @@ function status_bars(){
                     //appending machine name
                     $('#statuses').append("<p id=\"" +machines[i]['machineName']+ "\"> Machine Name: "  +machines[i]['machineName']+"</p>");
                     
-                    //display "machine out of order" status bar 
-                    if(machines[i]['status']==0){
-                        $('#statuses').append("<div class=\"progress\"> <div class=\"progress-bar-striped bg-danger\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Out of order</div></div>") 
-                    }
-
-                    //if machine is able to print
-                    else{
-
-                        //currently not in use
-                        if(machines[i]['inUse']==0){
-                            $('#statuses').append("<div class=\"progress\"> <div class=\"progress-bar-striped bg-info\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\">Not performing job</div></div>") 
-                        }
-                        
-                        //currently in use, inUse == 1
-                        else{
-
-                            //for the details of project being printed
-                            var matchedProject;
-
-                            //not inefficient double for loop since only select projects that don't have end time
-                            for(var j=0;j<projects.length;j++){
-                                if(projects[j]['machine']===machines[i]['machineName']){
-                                    
-                                    //save which project is being printed by this machine
-                                    matchedProject = projects[j];
-
-                                    //adding project print information next to machine name
-                                    var el = document.getElementById(machines[i]['machineName']);
-                                    el.innerHTML+= " ----- Started: " + projects[j]['startTime'] + " ----- By: " + projects[j]['userID'];
-
-                                    //no need to check other projects if match is found
-                                    break;
-                                }
-                            }
-
-                            //time variables
-                            var start = new Date(matchedProject['startTime']);
-                            var eta = new Date(matchedProject['eta']);
-                            var current = new Date();
-
-                            //if project start time is AFTER current time
-                            if(start > current){
-                                $('#statuses').append("<div class=\"progress\"> <div class=\"progress-bar-striped bg-warning\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\">Print not started yet</div></div>");
-                            }
-
-                            //if current time is AFTER end time
-                            else if(current > eta){
-                                $('#statuses').append("<div class=\"progress\"> <div class=\"progress-bar-striped bg-success\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\">Print finished already</div></div>");
-                            }
-
-                            //currently printing
-                            else{
-                                var totalTime = eta-start;
-                                var timeElapsed = current-start;
-                                var percentage = timeElapsed/totalTime * 100;
-                                $('#statuses').append("<div class=\"progress\"> <div class=\"progress-bar-striped progress-bar-animated bg-success\"  role=\"progressbar\" style=\"width: "+ percentage+"%\" aria-valuenow=\"50\" aria-valuemin=\"0\" aria-valuemax=\"100\">"+percentage+"</div></div>");
-                            }
-                        }
-                    }
+                    //default status bar
+                    $('#statuses').append("<div class=\"progress\"><div class=\"progress-bar-striped bg-info\" id=\"" + machines[i]['machineName'] + "_percentage\" role=\"progressbar\" style=\"width:100%\" aria-valuenow=\"100\"aria-valuemin=\"0\" aria-valuemax=\"100\"></div></div>");
                     
                 }
             }
+            //initial call
+            updateStatusBars(machines,projects);
+            //repeating call
+            setInterval(function () { updateStatusBars(machines, projects);},3000)
         }); 
-        
-        
-        
     });
+}
+function updateStatusBars(machines,projects) {
+    for (var i = 0; i < machines.length; i++) {
+        // console.log(machines[i]);
+        if (typeof machines[i] !== "undefined") {
 
-    
+            //variable to hold "[machineName]_percentage"
+            var elem = document.getElementById(machines[i]['machineName'] + '_percentage');
+
+            //display "machine out of order" status bar  
+            if (machines[i]['status'] == 0) {
+                elem.setAttribute('class', 'progress-bar-striped bg-danger');
+                elem.setAttribute('aria-valuenow', '100');
+                elem.innerHTML="Out of order";
+            }    
+
+            //if machine is able to print
+            else {
+
+                //currently not in use
+                if (machines[i]['inUse'] == 0) {
+
+                    elem.setAttribute('class', 'progress-bar-striped bg-info');
+                    elem.innerHTML = "Not performing job";
+                }
+
+                //currently in use, inUse == 1
+                else {
+
+                    //for the details of the project being printed
+                    var matchedProject;
+                    
+                    //look through every project
+                    for (var j = 0; j < projects.length; j++){
+                        //TODO: ACCOUNT FOR COMPLETED PROJECTS, MAKE SURE THEY DONT PASS THIS IF STATEMENT
+                        if (!projects[j]['endTime']) {
+                            if (projects[j]['machine'] === machines[i]['machineName']) {
+                                matchedProject = projects[j];
+                                var el = document.getElementById(machines[i]['machineName']);
+                                el.innerHTML = " Machine Name: " + machines[i]['machineName'] + " ----- Started: " + projects[j]['startTime'] + " ----- By: " + projects[j]['userID'];
+                                 //time calculation
+                                var start = new Date(matchedProject['startTime']);
+                                var eta = new Date(matchedProject['eta']);
+                                
+                                var current = new Date();
+                                
+                                //if project start time is AFTER current time
+                                if (start > current) {
+                                    elem.setAttribute('class', 'progress-bar-striped bg-warning');
+                                    elem.setAttribute('aria-valuenow', '0');
+                                    elem.innerHTML = "Print not started yet";
+                                }
+
+                                //if current time is AFTER end time
+                                else if (current > eta) {
+                                    elem.setAttribute('class', 'progress-bar-striped bg-success');
+                                    elem.innerHTML = "Print finished already";
+                                    
+                                }
+                                    
+                                //under work currently
+                                else {
+                                    var totalTime = eta-start;
+                                    var timeElapsed = current-start;
+                                    var percentage = timeElapsed / totalTime * 100;
+                                    elem.setAttribute('class', 'progress-bar-striped progress-bar-animated bg-success');
+                                    elem.setAttribute('style', 'width: \"' + percentage + '\"%');
+                                    elem.setAttribute('aria-valuenow', '50')
+                                    elem.innerHTML = percentage.toFixed(2);
+                                }
+                                break;
+                            }
+                        }
+                        else {
+                            if (projects[j]['machine'] === machines[i]['machineName']) {
+                                matchedProject = projects[j];
+                                elem.setAttribute('class', 'progress-bar-striped bg-warning');
+                                elem.innerHTML="DONE, PLEASE COME IN";
+                            }
+                        }
+                    }
+                }
+            }
+        }            
+
+    }
 }
 
 //populate the webpage with information upon load
 $(document).ready(function(){
-
     //initial call
-    status_bars();
+    createStatusBars();
 
-    //repeating call every 10 seconds, uncomment for auto refresh
-    setInterval(status_bars,10000);
 });
