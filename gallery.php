fc41bd8a89e8dfec35769b5469a3742b42552d8a
MichaelvMorrison <morrim5@rpi.edu>
2018-12-02 22:52:08 -0500

    Fixed some titles (#37)

--- gallery.php
+++ gallery.php
@@ -5,12 +5,21 @@
   <?php include 'style.php';?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<body class="bg-secondary">
+<body class="bg-light">
   <?php include 'nav_bar.php';?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-center pt-3 display-4 text-primary">Forge Gallery</h1>
+        <p class="text-center">Showcasing the very best of student projects.</p>
+      </div>
+    </div>
+  </div>
   <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
   <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
   <div class="container" id="juicer">
-    <ul class="juicer-feed" data-feed-id="rpimakerspace" data-per="20" data-style="hip" data-filter="Instagram"><h1 class="referral"></h1></ul>
+    <ul class="juicer-feed" data-feed-id="rpimakerspace" data-style="polaroid" data-per="20" data-style="hip" data-filter="Instagram"><h1 class="referral"></h1></ul>
   </div>
   <?php include 'footer.php' ?>
 </body>
