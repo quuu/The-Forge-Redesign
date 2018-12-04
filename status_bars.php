5f49ded856c710216d23c5d4543bbdae823f6e49
MichaelvMorrison <morrim5@rpi.edu>
2018-12-04 12:21:22 -0500

    Styled status bars a little They still don't work though so I don't wanna mess with it too much

--- status_bars.php
+++ status_bars.php
@@ -1,16 +1,15 @@
 <!doctype html>
-<html lang="en">
+<html class="bg-secondary" lang="en">
 <head>
   <meta charset="utf-8">
   <title>Machines</title>
-
-  <!--Bootstrap-->
-  <link rel="stylesheet" type="text/css" href="./css/home.css">
   <?php include 'style.php';?>
 </head>
 
 <body class="bg-secondary">
-  <?php include 'nav_bar.php';?>
+  <div class="bg-secondary pt-3 p-2">
+    <?php include_once 'nav_bar.php';?>
+  </div>
   <div class="container">
     <div class="row">
         <div class="col-sm-9 col-md-9 col-lg-9 mx-auto">
