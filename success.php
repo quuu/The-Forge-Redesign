bdcb12c747001bd223db1f1e07715bf1dd83038a
Samantha Sprecace <samspre@github.com>
2019-01-17 16:32:28 -0500

    Added code for the email form, not working

--- success.php
+++ success.php
@@ -3,21 +3,69 @@
 
 <head>
   <?php include 'style.php';?>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <body class="bg-light">
   <div class="bg-secondary pt-3 p-2">
       <?php include 'nav_bar.php';?>
   </div>
+  <?php
 
-  <div class="container">
-      <div class="row">
-          <div class="col-md-12">
-              <h1 class="text-center pt-3 display-4 text-primary">Your email has been sent</h1>
-          </div>
-      </div>
-  </div>
+
+  $name;
+  $email;
+  $message;
+  $from;
+  $to = 'roberr5@rpi.edu';
+  $subject;
+  $errName;
+  $errEmail;
+  $errSubject;
+  $errMessage;
+  $result;
+
+
+
+  if (isset($_POST["submit"])) {
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+      $from = "From $name<$email>";
+      $subject = $_POST['subject'];
+
+      // Check if name has been entered
+      if (!$_POST['name']) {
+          $errName = 'Please enter your name';
+      }
+
+      // Check if email has been entered and is valid
+      if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+          $errEmail = 'Please enter a valid email address';
+      }
+
+      //Check if subject has been entered
+      if (!$_POST['subject']) {
+          $errSubject = 'Please enter the subject';
+      }
+
+      //Check if message has been entered
+      if (!$_POST['message']) {
+          $errMessage = 'Please enter your message';
+      }
+
+      // If there are no errors, send the email
+      if (!$errName && !$errEmail && !$errMessage && !$errSubject) {
+          if (mail ($to, $subject, $message, $from)) {
+              $result='<div class="alert alert-success">Your message has been sent</div>';
+              echo $result;
+          } else {
+              $result='<div class="alert alert-danger">There was an error sending your message. Please try again later</div>';
+              echo $result;
+          }
+      }
+  }
+  ?>
 
   <?php include 'footer.php';?>
 </body>
