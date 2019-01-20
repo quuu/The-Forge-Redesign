9b2fe1de6683f9b8b593bec0a12c72903988bddb
Michael Morrison <morrim5@rpi.edu>
2019-01-19 21:59:23 -0500

    Added Some options for the donate button for now

--- index.php
+++ index.php
@@ -34,30 +34,29 @@
           </div>
         </header>
           <!-- Displays on large screens -->
-          <div class="container m-auto d-none d-sm-block">
+          <div class="container overflow-hidden m-auto d-none d-sm-block">
               <div class="row">
                   <div class="col-sm-12">
                       <h1 class="cover-heading p-0 m-0">THE FORGE</h1>
                   </div>
               </div>
-            <div class="row">
-              <div class="col-sm-12">
+              <div class="row">
+                <div class="col-sm-12">
                   <hr class="cover-hr p-0 mx-auto mb-3" />
+                </div>
               </div>
-            </div>
               <div class="row">
                   <div class="col-sm-12">
                       <p class="roboto text-justify cover-description">The Forge intends to provide the environment to create a collaborative design community on campus, promoting communication, critical thinking, and STEAM (Science, Technology, Engineering, Arts, Mathematics) ideals. Through this, the Forge will provide easy and everyday access to rapid prototyping tools, communication and collaboration tools, and a casual area to work. The Forge will hold workshops, design competitions, and speakers involved in the Making Movement.</p>
+                  </div>
               </div>
-            </div>
-              <!-- needs to be converted to work on mobile -->
-              <div class="container m-auto d-none d-sm-block">
-                  <h2 class="" style="text-align: left; color: gold; font-weight: bold" >WeR Gold</h2>
-                  <p class="roboto pb-5" style="text-align: left">Support the Forge at weR Gold!</p>
-                  <a class="btn btn-primary" style="background-color: gold; color: black; border: 2px gold; float:left; margin-top: -70px" href="https://impact.rpi.edu/project/13495">Donate
-                  <a href="https://impact.rpi.edu/project/13495" target="_blank" class="virtual_link"></a>
+              <div class="row">
+                <div class="text-center container m-auto p-3">
+                    <h2 class="text-warning font-weight-bold" >WeR Gold</h2>
+                    <p class="roboto">Support the Forge at weR Gold!</p>
+                    <a class="btn btn-primary btn-warning" href="https://impact.rpi.edu/project/13495" target="_blank">Donate</a>
+                </div>
               </div>
-              <!-- --------------------------- -->
           </div>
           <!-- Displayes on large screens -->
           <!-- Displays on mobile screens -->
@@ -86,7 +85,7 @@
               <i class="d-block  fa fa-5x fa-globe"></i>
             </div>
             <div class="col-sm-9">
-              <h3 class="">Find us Physically</h3>
+              <h3 class="">Find Us Physically</h3>
               <p class="">We are located in the CII 2037. &nbsp;Take the elevators down in the Low building to the second floor. &nbsp;If the sign is flipped to OPEN, come on in. &nbsp;The open hours are linked below</p>
               <a class="btn btn-primary" href="hours.php">Hours of Operation
                 <br> </a>
@@ -99,7 +98,7 @@
               <i class="d-block  fa fa-5x fa-mouse-pointer"></i>
             </div>
             <div class="col-sm-9">
-              <h3 class="">Find us Virtually</h3>
+              <h3 class="">Find Us Virtually</h3>
               <ul class="list-group">
                 <a href="https://www.facebook.com/RPIMakerSpace/" target="_blank" class="virtual_link">
                   <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-info virtual_list_item">Facebook
@@ -115,6 +114,18 @@
             </div>
           </div>
         </div>
+      </div>
+      <div class="row w-75 m-auto pt-2 pb-4 d-block d-sm-none">
+        <hr />
+          <div class="col-md-5 align-self-center p-2">
+            <img class="card-img" src="weR.jpg">
+          </div>
+          <div class="col-md-7 p-2">
+            <h3>Support Us</h3>
+            <p class="">Contribute to our cause through the RPI weR Gold crowdfunding platform! 100% RPI-organized, weR Gold will match any and all of your donations. Help us reach our current goal of $6120! </p>
+            <a class="btn btn-gold align-middle gold text-dark" href="https://impact.rpi.edu/project/13495" target="_blank">Donate</a>
+          </div>
+      </div>
       </div>
     </div>
     </div>
